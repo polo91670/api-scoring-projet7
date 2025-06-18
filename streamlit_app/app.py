5d59bcbb851d2f32a6137115c63077a312adaf04
pvanyialu <pvanyialu@hotmail.com>
2025-06-18 23:28:10 +0200

    MAJ render.yaml

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -13,8 +13,8 @@ def load_client_ids():
 
 
 #URL de ton API FastAPI déployée sur Render
-API_URL = "https://api-scoring-projet7.onrender.com/predict_score"
-#API_URL = "http://localhost:8000/predict_score"
+#API_URL = "https://api-scoring-projet7.onrender.com/predict_score"
+API_URL = "http://localhost:8000/predict_score"
 
 st.title("Score Client - Projet 7")
 
@@ -61,11 +61,11 @@ client_ids = load_client_ids()
 selected_id = st.selectbox("Choisissez un identifiant client :", client_ids)
 
 #Bouton de prédiction
-if st.button("Prédire"):
+if st.button("Prédiction"):
     try:
         response = requests.post(API_URL, json={"client_id": selected_id})
-        st.write("Statut HTTP :", response.status_code)
-        st.write("Texte brut :", response.text)
+        #st.write("Statut HTTP :", response.status_code)
+        #st.write("Texte brut :", response.text)
 
         if response.status_code == 200:
             result = response.json()
